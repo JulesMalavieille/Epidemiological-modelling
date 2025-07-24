23ff18a1cb0624d13ed97eb9af890c7f1c5eb3ce
Jules <j.malavieille444@gmail.com>
2025-07-24 20:51:30 +0200

    Update and rename Modèle_PFZ.py to Model_PFZ.py

--- Model_PFZ.py
+++ Model_PFZ.py
@@ -8,12 +8,11 @@ time = np.linspace(0, 1000, 10000)
 A = np.linspace(0, 1, 100)
 B = np.linspace(0.1, 2.5, 100)
 C = np.linspace(0, 100, 100)
-a = 0.5 # Taux de croissance du phyto (0-1)
-b = 0.25 # Taux de broutage phyto -> Zoo (0-1)
-m = 0.25 # Taux de mortalité du zooplancton (0-1)
-c = 0.8 # Taux d'infection du phytoplancton (0-100)
-L = 1 # Temps de latence du virus avant mort du phyto (0.1-2.5)
-g = 2 # Taux de broutage préférentiel (0-100)
+a = 0.5 # Phytoplankton growth
+b = 0.25 # Grazing rate 
+m = 0.25 # Mortality rate of zooplankton
+c = 0.8 # Infection rate of phytoplankton
+L = 1 # Time before virus kill phytoplankton 
 
 Pi = 1
 Fi = 1
@@ -22,20 +21,20 @@ Zi = 1
 def modPFZ(t, Y):
     p, f, z = Y
     dp = a*p - (c/L)*p*f - b*p*z
-    df = (c/L)*p*f - f/L - b*f*z # gamma absent
-    dz = b*(p+f)*z - m*z**2  # gamma absent
+    df = (c/L)*p*f - f/L - b*f*z 
+    dz = b*(p+f)*z - m*z**2  
     return[dp, df, dz]
 
 
 def traj(Yini):
-    tfinal = 1000  # en jours
+    tfinal = 1000  # in days
     pini, fini, zini = Yini
     t = np.linspace(0, tfinal, 10000)
     S = solve_ivp(modPFZ, [0, tfinal], [pini, fini, zini], t_eval=t)
     return S
 
 
-"""Calcul du broutage en fonction d'un paramètre dans les conditions normales"""
+"""Grazing as a function of 1 parmeter"""
 # brouta_param = []
 # for i in range(len(A)):
 #     a = 0.5
@@ -44,11 +43,11 @@ def traj(Yini):
 #     c = 1
 #     L = 1
 #     S = traj([Pi, Fi, Zi])
-#     morta_tot = b * S.y[0][-1] * S.y[2][-1]+ b * S.y[1][-1] * S.y[2][-1] + (S.y[1][-1] / L) # gamma absent
+#     morta_tot = b * S.y[0][-1] * S.y[2][-1]+ b * S.y[1][-1] * S.y[2][-1] + (S.y[1][-1] / L) 
 #     brouta = ((b * S.y[0][-1] * S.y[2][-1]+ b * S.y[1][-1] * S.y[2][-1]) / morta_tot) * 100
 #     brouta_param.append(brouta)
     
-"""Calcul du broutage en fonction de deux paramètres dans les conditions normales"""
+"""Grazing as a function of 2 parmeters"""
 # brouta_param = np.zeros((len(A), len(A)))
 # for i in range(len(A)):
 #     for j in range(len(A)):
@@ -58,11 +57,11 @@ def traj(Yini):
 #         c = 1
 #         L = 1
 #         S = traj([Pi, Fi, Zi])
-#         morta_tot = b * S.y[0][-1] * S.y[2][-1]+ b * S.y[1][-1] * S.y[2][-1] + (S.y[1][-1] / L) # gamma absent
+#         morta_tot = b * S.y[0][-1] * S.y[2][-1]+ b * S.y[1][-1] * S.y[2][-1] + (S.y[1][-1] / L) 
 #         brouta = ((b * S.y[0][-1] * S.y[2][-1]+ b * S.y[1][-1] * S.y[2][-1]) / morta_tot) * 100
 #         brouta_param[i, j] = brouta
 
-"""Calcul de biomasse phytoplancton en fonction d'un paramètre"""
+"""Phytoplanktonic biomass as a function of 1 parmeter"""
 biomasse = []
 for i in range(len(A)):
     a = 0.5
@@ -73,17 +72,17 @@ for i in range(len(A)):
     S = traj([Pi, Fi, Zi])
     biomasse.append(S.y[0][-1])
 
-"""Calcul du broutage en fonction de temps"""
+"""Grazing as a function of time"""
 # time = np.linspace(0, 1000, 10000)
 # broutage = []
 # for i in range(len(S.y[0])):
-#     morta_tot = b * S.y[0][i] * S.y[2][i]+ b * S.y[1][i] * S.y[2][i] + (S.y[1][i] / L) # gamma absent
-#     brouta = ((b * S.y[0][i] * S.y[2][i]+ b * S.y[1][i] * S.y[2][i]) / morta_tot) * 100   # gamma absent
+#     morta_tot = b * S.y[0][i] * S.y[2][i]+ b * S.y[1][i] * S.y[2][i] + (S.y[1][i] / L) 
+#     brouta = ((b * S.y[0][i] * S.y[2][i]+ b * S.y[1][i] * S.y[2][i]) / morta_tot) * 100   
 #     broutage.append(brouta)
 
 # brout_tot = np.sum(broutage) / len(broutage)
 
-"""Méthode pour trouver numériquement la période d'un cycle limite"""
+"""Method to find numerically the period of a limit cycle"""
 # def periode_lim(Xi, tps_lim):
 #     X = Xi[tps_lim:]
 #     x1 = min(X)
@@ -107,7 +106,7 @@ for i in range(len(A)):
 # print()
 # print("La période du cycle limite est de ", periode, "jours. ")
 
-""" Dynamique du modèle """
+""" General model dynamic """
 # plt.figure(1)
 # plt.plot(S.y[0], S.y[1], color="red")
 # plt.xlabel("Phyotplancton sain en $mmol-C.m^{-3}$", fontsize=12)
@@ -138,20 +137,20 @@ for i in range(len(A)):
 # plt.title("Dynamique global du modèle PFZ", fontsize=18)
 
 
-""" Broutage en fonction du temps """
+""" Grazing as a function of time """
 # plt.figure(5)
 # plt.plot(time, broutage)
 # plt.xlabel("Time")
 # plt.ylabel("Taux de broutage")
 
-""" Broutage en fonction d'1 paramètres"""
+""" Grazing as a function of 1 parameter"""
 # plt.figure(6)
 # plt.plot(A, brouta_param, c="palevioletred", linewidth=2.5)
 # plt.ylabel("Broutage sur la mortalité totale en %", fontsize=12)
 # plt.xlabel("Valeur de a en $j^{-1}$", fontsize=12)
 # plt.title("Fraction du broutage dans la mortalité totale en fonction de la croissance du phytoplancton (a)", fontsize=18)
 
-""" Broutgage en fonction de 2 paramètres """
+""" Grazing as a function of 2 parameters """
 # plt.figure(7)
 # plt.pcolor(A, A, brouta_param, cmap="viridis")
 # plt.colorbar(label="broutage sur la mortalité totale en %")
@@ -159,27 +158,24 @@ for i in range(len(A)):
 # plt.ylabel("mortalité du zooplancton en (mmol-C.m^-3)^-1.j^-1 (m)")
 # plt.title("Fraction du broutage dans la mortalité totale en fonction de b et m", fontsize=15)
 
-"""Biomasse de phytoplancton en fonction d'un paramètre"""
+"""Biomasse of phytoplancton as a function of 1 parameter"""
 plt.figure(8)
 plt.plot(A, biomasse, color="forestgreen", linewidth=2.5)
-plt.xlabel("Mortality rate of zooplancton (m) $(mmol-C.m^{-3})^{-1}.j^{-1}$", fontsize=15) #$(mmol-C.m^{-3})^{-1}.
+plt.xlabel("Mortality rate of zooplancton (m) $(mmol-C.m^{-3})^{-1}.j^{-1}$", fontsize=15) 
 plt.ylabel("Phytoplancton biomass ($mmol-C.m^{-3}$)", fontsize=15)
 plt.title("Biomass of phytoplancton as a fonction of m", fontsize=20)
 
-""" Retour sur le terminal"""
-
+""" General return """
 S = traj([Pi, Fi, Zi])
 
 p = S.y[0][-1]
 f = S.y[1][-1]
 z = S.y[2][-1]
+
 print()
-print("A la fin des 10 000 itérations :")
-print()
-print("Phytoplancton : ", p)
-print("Phytoplancton infécté :", f)
-print("Zooplancton : ", z)
-# print("Mortalité par le broutage : ", brout_tot, "%")
+print("Phytoplankton : ", p)
+print("Infected phytoplankton :", f)
+print("Zooplankton : ", z)
 
 
 
